d3cbad70e8ced7b19ec164286d41725997d4e982
user <user@users-MacBook-Pro.local>
2019-09-13 14:38:04 +0100

    added remove pet by name function

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,17 +1,17 @@
 def pet_shop_name(pet_shop)
-return pet_shop[:name]
+  return pet_shop[:name]
 end
 
 def total_cash(pet_shop)
-return pet_shop[:admin][:total_cash]
+  return pet_shop[:admin][:total_cash]
 end
 
 def   add_or_remove_cash(pet_shop, first_amount)
-pet_shop[:admin][:total_cash] += first_amount
+  pet_shop[:admin][:total_cash] += first_amount
 end
 
 def pets_sold(pet_shop)
-return pet_shop[:admin][:pets_sold]
+  return pet_shop[:admin][:pets_sold]
 end
 
 def increase_pets_sold(pet_shop, number)
@@ -24,20 +24,28 @@ end
 
 def pets_by_breed(pet_shop, breed)
   pets_found = []
-for pet in pet_shop[:pets]
-  if pet[:breed] == breed
-    pets_found.push(pet)
-end
+  for pet in pet_shop[:pets]
+    if pet[:breed] == breed
+      pets_found.push(pet)
+    end
   end
   return pets_found
 end
 
 def find_pet_by_name(pet_shop, name)
-for pet in pet_shop[:pets]
-if pet[:name] == name
-  return pet
+  for pet in pet_shop[:pets]
+    if pet[:name] == name
+      return pet
 
+    end
+  end
+  return nil
 end
-end
-return nil
+
+def remove_pet_by_name(pet_shop, name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == name
+      pet.shift()
+    end
+  end
 end
