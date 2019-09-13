fa7ed8eb3eab1dea583f6db8ad1d1194602556d4
user <user@users-MacBook-Pro.local>
2019-09-13 13:56:04 +0100

    added stock_count function

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -6,6 +6,18 @@ def total_cash(pet_shop)
 return pet_shop[:admin][:total_cash]
 end
 
-def   add_or_remove_cash(pet_shop, amount)
-pet_shop[:admin][:total_cash] += amount
+def   add_or_remove_cash(pet_shop, first_amount)
+pet_shop[:admin][:total_cash] += first_amount
+end
+
+def pets_sold(pet_shop)
+return pet_shop[:admin][:pets_sold]
+end
+
+# def increase_pets_sold( sold , pet_shop)
+#  input_pets_sold = pet_shop[:admin][:pets_sold] + sold
+# end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].length()
 end
